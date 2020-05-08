9fb0f0775b565fb5674f3e0ddb945f282c7ad55d
HadiKhoirudin <347extreme@gmail.com>
2020-05-08 17:24:38 +0700

    Fix Audio Port Andronix

---
+++ audio.sh
@@ -0,0 +1,4 @@
+#!/data/data/com.termux/files/usr/bin/bash
+pulseaudio --start
+sleep 5
+pactl load-module module-simple-protocol-tcp rate=48000 format=s16le channels=2 source=auto_null.monitor record=true port=8000 listen=127.0.0.1
