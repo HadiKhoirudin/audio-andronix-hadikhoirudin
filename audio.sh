4b7e236d8e36d4ce05b7e5a2b2fd897bf10a482f
HadiKhoirudin <347extreme@gmail.com>
2020-05-08 17:53:45 +0700

    Add file fix audio

--- audio.sh
+++ audio.sh
@@ -1,4 +1,5 @@
 #!/data/data/com.termux/files/usr/bin/bash
+# Created By Hadi Khoirudin
 pulseaudio --start
 sleep 5
 pactl load-module module-simple-protocol-tcp rate=48000 format=s16le channels=2 source=auto_null.monitor record=true port=8000 listen=127.0.0.1
